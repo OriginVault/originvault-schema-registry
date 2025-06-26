1c9c4690f80a189016d7d1825ae0348d6e829955
lnispel <lnispel@me.com>
2025-06-26 02:39:57 -0700

    docs-site

--- docs-site/src/test/Layout.test.tsx
+++ docs-site/src/test/Layout.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import Layout from '../components/Layout.jsx'
